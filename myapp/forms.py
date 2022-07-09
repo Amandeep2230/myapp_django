cdc47192a03ad7e77bc386505c5c4574c6c3c2a8
Shobhit Narayan <39194490+nocreepsho@users.noreply.github.com>
2022-07-09 15:52:37 -0400

    Update forms.py

--- myapp/forms.py
+++ myapp/forms.py
@@ -1,11 +1,11 @@
 from django import forms
-<<<<<<< HEAD
+
 from .models import Order
 from django.core.validators import MinValueValidator
-=======
+
 from django.core.validators import MinValueValidator
 from .models import Order
->>>>>>> 290c1128560543449f6500c4cb0b2ea2960667db
+
 
 
 class InterestForm(forms.Form):
@@ -13,7 +13,7 @@ class InterestForm(forms.Form):
         (1, "Yes"),
         (0, "No")
     ]
-<<<<<<< HEAD
+
     interested = forms.ChoiceField(widget=forms.RadioSelect, choices=CHOICES)
     levels = forms.IntegerField(widget=forms.NumberInput, initial=1, validators=[MinValueValidator(1)])
     comments = forms.CharField(widget=forms.Textarea, label='Additional Comments', required=False)
@@ -31,8 +31,8 @@ class OrderForm(forms.ModelForm):
             'student': forms.RadioSelect(),
             'order_date': forms.SelectDateWidget(),
         }
-=======
+        
     interested = forms.ChoiceField(widgets=forms.RadioSelect, choices=CHOICES)
     levels = forms.IntegerField(widget=forms.NumberInput, initial=1, validators=[MinValueValidator(1)])
     comments = forms.CharField(widget=forms.Textarea, label='Additional Comments', blank=True)
->>>>>>> 290c1128560543449f6500c4cb0b2ea2960667db
+
