df2119d502a884dfe3ca3be19e4ec297e773c18b
Aman <aman@Amans-MacBook-Air.local>
2022-08-04 11:37:37 -0400

    final q1

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,10 +1,20 @@
+
 from django.contrib import admin
 from django.db import models
 from .models import Topic, Course, Student, Order
 
 # Register your models here.
 
-admin.site.register(Topic)
+#final project part 1
+class CourseInline(admin.TabularInline):
+    model = Course
+
+class TopicAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category')
+    inlines = [
+        CourseInline,
+    ]
+admin.site.register(Topic, TopicAdmin)
 admin.site.register(Course)
 admin.site.register(Student)
 admin.site.register(Order)
